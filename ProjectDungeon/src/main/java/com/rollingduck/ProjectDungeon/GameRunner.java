1cba1f112d03f9377b64cd53c8d37ae58e62915c
Ben Woods <benwoods2650@gmail.com>
2015-05-04 21:57:34 +0100

    Remove premade class, added game runner

---
+++ ProjectDungeon/src/main/java/com/rollingduck/ProjectDungeon/GameRunner.java
@@ -0,0 +1,18 @@
+package com.rollingduck.ProjectDungeon;
+
+/**
+ * Hello world!
+ *
+ */
+public class GameRunner 
+{
+    public static void main( String[] args )
+    {
+        GameRunner runner = new GameRunner();
+        runner.run();
+    }
+    
+    public void run(){
+    	System.out.println( "Game Running" );
+    }
+}
