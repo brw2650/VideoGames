8fec82b717720b65f6669cbac37bf3cf1da77767
Ben Woods <benwoods2650@gmail.com>
2015-05-08 19:48:59 +0100

    Remove comment

--- ProjectDungeon/src/main/java/com/rollingduck/projectdungeon/entities/Player.java
+++ ProjectDungeon/src/main/java/com/rollingduck/projectdungeon/entities/Player.java
@@ -6,7 +6,6 @@ public class Player extends Actor {
 
 	public Player(Coordinates coordinates, int health) {
 		super(coordinates, health);
-		// TODO Auto-generated constructor stub
 	}
 
 	public int getEntityId() {
